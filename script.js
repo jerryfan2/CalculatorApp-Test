1c2626616654ed9731bb3a026d547dc7593236d8
Jerry Fan <jfjf22@students.cs.ubc.ca>
2022-08-26 17:01:19 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function clearScreen() {
+    let displayBox = document.getElementById("result");
+    displayBox.value = ""
+}
+
+function display(value) {
+    let displayBox = document.getElementById("result");
+    displayBox.value += value;
+}
+
+function calculate() {
+    let displayBox = document.getElementById("result").value;
+    let answer = eval(displayBox)
+    document.getElementById("result").value = answer;
+}
+
+// 1. clear function
+// 2. calculate function
+// 3. display function
